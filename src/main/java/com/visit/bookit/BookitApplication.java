91ae66ed68a23666304ab35bd224407e72b7ffc9
wszczesniak <szczesniakwiktoria355@gmail.com>
2023-11-17 11:35:34 +0100

    Initial commit

---
+++ src/main/java/com/visit/bookit/BookitApplication.java
@@ -0,0 +1,13 @@
+package com.visit.bookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visit/bookit/BookitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visit.bookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
