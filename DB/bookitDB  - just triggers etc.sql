0c77c4ac9589c2830d36cacc238dcf76fd975c92
weraa02 <wera.chmiela123@gmail.com>
2023-12-11 16:09:01 +0100

    Add triggerrs etc part2
    Added some procedures code like: adding calendarevent with assignaion of employee, adding new usser with password, adding new calendarevent and new employee

--- DB/bookitDB  - just triggers etc.sql
+++ DB/bookitDB  - just triggers etc.sql
@@ -63,3 +63,86 @@ BEGIN
 	END IF;
 END $$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_caleventWITHevemployee;
+DELIMITER $$
+CREATE PROCEDURE add_caleventWITHevemployee(
+IN PeventdetailsID INT, PeventDateTime DATETIME, PemployeeID INT)
+BEGIN
+DECLARE eID INT;
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        	BEGIN
+            		ROLLBACK;
+            		RESIGNAL;
+        	END;
+	START TRANSACTION;
+INSERT INTO calendarevents (eventdetailsID, eventDateTime) VALUES (PeventdetailsID, PeventDateTime);
+   		SET eID = LAST_INSERT_ID();
+		INSERT INTO eventemployee (calendarEventsID, employeeID) VALUES (eID, PemployeeID);
+	COMMIT;
+END$$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS add_userWITHpassword;
+DELIMITER $$
+CREATE PROCEDURE add_userWITHpassword(
+IN PfirstName VARCHAR(255), PlastName VARCHAR(255), PemailAddress VARCHAR(255),   PphoneNumber VARCHAR(255), PpassHash VARCHAR(255))
+BEGIN
+	   DECLARE uID INT;
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        	BEGIN
+            		ROLLBACK;
+            		RESIGNAL;
+        	END;
+	START TRANSACTION;
+INSERT INTO users (firstName, lastName, emailAddress, phoneNumber) VALUES (PfirstName, PlastName, PemailAddress, PphoneNumber);
+   		SET uID = LAST_INSERT_ID();
+		INSERT INTO userpassword (userID, passwordHash) VALUES (uID, PpassHash);
+	COMMIT;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS add_usereventWITHquestion;
+DELIMITER $$
+CREATE PROCEDURE add_usereventWITHquestion(
+IN PuserID INT, PcalendarEventsID INT)
+BEGIN
+DECLARE compID INT;
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        	BEGIN
+            		ROLLBACK;
+            		RESIGNAL;
+        	END;
+	START TRANSACTION;
+SELECT ed.companyID INTO compID FROM eventdetails ed
+JOIN calendarevents ce ON ed.eventdetailsID = ce.eventdetailsID
+WHERE ce.calendarEventsID = PcalendarEventsID;
+    		INSERT INTO toaccept(companyID, userID, calendarEventsID) VALUES (compID, PuserID, PcalendarEventsID);
+INSERT INTO userevents (userID, calendarEventsID, status) VALUES (PuserID, PcalendarEventsID, 'waiting');
+	COMMIT;
+END$$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS add_caleventANDemployee;
+DELIMITER $$
+CREATE PROCEDURE add_caleventANDemployee(
+IN  PeventdetailsID INT, PeventDateTime DATETIME, PcompanyID int, PemployeeFName VARCHAR(255), PemployeeLName VARCHAR(255))
+BEGIN
+    	DECLARE cID INT;
+    	DECLARE eID INT;
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        	BEGIN
+            		ROLLBACK;
+            		RESIGNAL;
+        	END;
+START TRANSACTION;
+INSERT INTO calendarevents (eventdetailsID, eventDateTime) VALUES (PeventdetailsID, PeventDateTime);
+    		SET cID = LAST_INSERT_ID();
+INSERT INTO employee (companyID, employeeFName, employeeLName) VALUES (PcompanyID, PemployeeFName, PemployeeLName);
+    		SET eID = LAST_INSERT_ID();
+		INSERT INTO eventemployee (calendarEventsID, employeeID) VALUES (cID, eID);
+COMMIT;
+END$$
+DELIMITER ;
